d32f9077d0338b8c890567fd8c37a628e8d00d7f
d-s-t <davidst360@gmail.com>
2023-04-24 20:09:55 +0300

    change the measurement variable range. fix figure plotting (to the same figure, not to the top one) remove unnecessary comments. comment out the loop for sampleCurrent.

--- vortices.m
+++ vortices.m
@@ -1,4 +1,4 @@
-SAVE_PATH = '.\2nd_day\vortices\'; %chosen folder
+SAVE_PATH = '.\day3\vortices\'; %chosen folder
 MINIMUM_TEMPERATURE = 80;
 
 temperature_table = load('temperature_conversion.mat', 'table');
@@ -27,7 +27,7 @@ try
     heatCurr=0;
     heatVolt=0;
     
-    pauseInterval=0.25; % Seconds
+    pauseInterval=0; % Seconds
     
     %Saving to file in a chosen folder
     savePath=SAVE_PATH;
@@ -54,10 +54,13 @@ try
     time_limit = 120000;
     pause(pauseInterval);
     figure
+    a=gca;
+    xlabel("\propto H");
+    ylabel("sample voltage");
     hold on
-    for sampleCurrent=0.2:0.025:0.45
-        fprintf(sampleCurrentDevice,sprintf ('INST P6V; CURR %f; INST P25V; VOLT %f; CURR %f',sampleCurrent,heatVolt,heatCurr));
-        for coilCurrent=3:0.1:6
+    %for sampleCurrent=0.2:0.025:0.45
+        %fprintf(sampleCurrentDevice,sprintf ('INST P6V; CURR %f; INST P25V; VOLT %f; CURR %f',sampleCurrent,heatVolt,heatCurr));
+        for coilCurrent=0:0.08:6.4
         fprintf(coilCurrentDevice,sprintf ('APPL %f, %f',coilVolt,coilCurrent));%Setting Field current
     
         pause(pauseInterval);
@@ -82,45 +85,15 @@ try
         coilPreviousMeas = coilCurrentMeas;
         coilCurrentMeas = readMeas(coilCurrentDevice);
         
-        plot(sampleCurrentMeas,sampleVoltage,'b.','MarkerSize',20)
+        plot(a, coilCurrent,sampleVoltage,'b.','MarkerSize',20)
+        title(a, sprintf("temp: %f K", temperature));
         
         dlmwrite (fname,[time,tempResistance,temperature,...
             sampleVoltage,sampleCurrentMeas,...
             coilCurrentMeas],'-append');
-        
-
-        %
-%          fprintf(coilCurrentDevice,sprintf ('APPL %f, %f',0,0));%Setting Field current
-%         
-%         pause(pauseInterval);
-%         time=etime(clock,startTime);
-%         
-%         % Read temperature
-%         fprintf(tempResDevice, 'READ?');
-%         tempResistance = readMeas(tempResDevice);
-%         temperature = interp1(temperature_table(:,1),temperature_table(:,2),tempResistance);
-%        
-%         % Read sample voltage
-%         fprintf(sampleVoltageDevice, 'READ?');
-%         sampleVoltage = readMeas(sampleVoltageDevice);
-%         
-%         % Read sample current
-%         fprintf(sampleCurrentDevice, 'INST P6V; CURRENT?');
-%         sampleCurrentMeas = readMeas(sampleCurrentDevice);
-%         fprintf(sampleCurrentDevice, 'INST P25V;');
-%         
-% 
-%         
-%         plot(coilCurrentMeas,sampleVoltage,'r.','MarkerSize',20)
-%         
-%         dlmwrite (fname,[time,tempResistance,temperature,...
-%             sampleVoltage,sampleCurrentMeas,...
-%             coilCurrentMeas],'-append');
-        
-        
-        %
         end
-    end
+    %end
+    
     %setting the currents back to zero
     fprintf(sampleCurrentDevice,sprintf ('INST P6V; CURR %f; INST P25V; CURR %f',0,0));
     fprintf(coilCurrentDevice,'OUTPUT OFF');
