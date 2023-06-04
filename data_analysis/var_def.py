fb200387958082704e0abeac2123c376e9db4422
d-s-t <david.shem-tov@mail.huji.ac.il>
2023-06-04 13:07:07 +0300

    add some functions

--- data_analysis/var_def.py
+++ data_analysis/var_def.py
@@ -14,16 +14,25 @@ AXIS_TITLE = {"Temperature(K)": r"$ \text{Temperature} \left[ K \right] $",
               "H": r"$ \text{Applied Magnetic Field} \left[ G \right] $"}
 
 
-def exp(x, y0, k, b, phi):
-    return y0 * np.exp(k * x + phi) + b
+def exp(x, y0, k, b):
+    return y0 * np.exp(k * x) + b
 
 
 def line(x, m, b):
     return x * m + b
 
 
-def exp_heavy_line(x, y0, k, b0, phi, x0, m):
-    return np.where(x < x0, exp(x, y0, k, b0, phi), line(x - x0, m, exp(x0, y0, k, b0, phi)))
+def exp_heavy_line(x, y0, k, b0, x0, m):
+    return np.where(x < x0, exp(x, y0, k, b0), line(x - x0, m, exp(x0, y0, k, b0)))
 
 
-FUNCTIONAL_RELATION = {"exp": exp, "line": line, "exp_heavy_line": exp_heavy_line}
+def ih_surface(x, i0, b0, v0, k, b1, x0, m1):
+    return exp(x[:, 0], v0, 1 / i0, b0) * exp_heavy_line(x[:, 1], 1, k, b1, x0, m1)
+
+
+def exp_surface(x, v0, k0, k1, b):
+    return v0 * np.exp(k0 * x[:, 0] + k1 * x[:, 1]) + b
+
+
+FUNCTIONAL_RELATION = {"exp": exp, "line": line, "exp_heavy_line": exp_heavy_line, "ih_surface": ih_surface,
+                       "exp_surface": exp_surface}
